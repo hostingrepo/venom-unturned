34984b2a24e39a33cb5ea8911d9c679dc0566d4b
Edward Brook <31439728+Edbtvplays@users.noreply.github.com>
2019-01-03 18:18:14 +0000

    Updated Entrypoint for new rocket release

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ cd /home/container
 # Update Unturned Server
 ./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 32 +login "${STEAM_USER}" "${STEAM_PASS}" +force_install_dir /home/container +app_update 304930 +quit
 echo "Downloading RocketMod..."
-curl -o Rocket.zip "https://ci.rocketmod.net/job/Rocket.Unturned%20Linux/lastSuccessfulBuild/artifact/Rocket.Unturned/bin/Release/Rocket.zip"
+curl -o Rocket.zip "https://ci.rocketmod.net/job/Rocket.Unturned/lastSuccessfulBuild/artifact/Rocket.Unturned/bin/Release/Rocket.zip"
 unzip -o -q Rocket.zip
 mv /home/container/Scripts/Linux/RocketLauncher.exe /home/container/RocketLauncher.exe
 
@@ -28,4 +28,3 @@ echo ":/home/container$ ${MODIFIED_STARTUP}"
 # Run the Server
 ${MODIFIED_STARTUP}
 echo "If there was an error above when trying to stop your server, it can usually be ignored."
-
